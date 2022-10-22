8b85ce17e91d373e8f8ed976a059d42bfec070d1
Chan Lee <clee09818@gmail.com>
2022-10-22 01:10:48 -0400

    commit tree f07c7628281fab7de0b537f45b8d5f0396597e84

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,29 @@
 // create your App component here
+import React, { useEffect, useState } from 'react';
+
+function App(){
+
+    const [dogPic, setDogPic] = useState(null);
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data.message)
+                setDogPic(data.message)
+            })
+    },[])
+
+
+    if (!dogPic){
+        return <p>Loading...</p>
+    }
+
+    return (
+        <div>
+            <img src={dogPic} alt="A Random Dog" />
+        </div>
+    )
+}
+
+export default App;
